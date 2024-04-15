187118c65961b3ea42d1abf2dba264caa4fd5c58
Crosnierpaul <crosnier.paul@outlook.fr>
2024-04-15 14:31:05 +0200

    blog version 3

--- blog/js/admin.js
+++ blog/js/admin.js
@@ -59,12 +59,34 @@ document.getElementById('deleteLastArticle').addEventListener('click', function(
     var articles = JSON.parse(localStorage.getItem('articles')) || [];
 
     if (articles.length > 0) {
-        console.log("yo")
         articles.pop(); // Supprimer le dernier article
         localStorage.setItem('articles', JSON.stringify(articles));
         alert('Le dernier article a été supprimé avec succès !');
     } else {
-        console.log("yo")
         alert('Il n\'y a aucun article à supprimer !');
+    }
+});
+
+// Gérer le clic sur le bouton "Supprimer l'article"
+document.getElementById('deleteArticle').addEventListener('click', function() {
+    var articleTitle = document.getElementById('articleTitle').value.trim();
+    var articles = JSON.parse(localStorage.getItem('articles')) || [];
+
+    if (articleTitle === '') {
+        alert('Veuillez saisir un titre d\'article.');
+        return;
+    }
+
+    // Rechercher l'indice de l'article avec le titre spécifié
+    var index = articles.findIndex(function(article) {
+        return article.title === articleTitle;
+    });
+
+    if (index !== -1) {
+        articles.splice(index, 1); // Supprimer l'article
+        localStorage.setItem('articles', JSON.stringify(articles));
+        alert('L\'article a été supprimé avec succès !');
+    } else {
+        alert('Aucun article trouvé avec ce titre.');
     }
 });
