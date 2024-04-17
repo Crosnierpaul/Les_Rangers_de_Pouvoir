545af5ca6bd5d13c741f166b1979e409838d0fd0
Crosnierpaul <crosnier.paul@outlook.fr>
2024-04-17 10:33:35 +0200

    MODIFICATION OVER 9000

---
+++ blogV3/models/Article.js
@@ -0,0 +1,19 @@
+// models/Article.js
+
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Article', articleSchema);
---
+++ blogV3/server.js
@@ -0,0 +1,31 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const articleRoutes = require('./routes/articleRoutes');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Middleware
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/articles', articleRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
