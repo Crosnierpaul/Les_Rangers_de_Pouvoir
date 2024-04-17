288fe5edc300bea0834fff654590f93a8b30f399
Crosnierpaul <crosnier.paul@outlook.fr>
2024-04-17 09:36:17 +0200

    blogV2 version test

---
+++ blogV2/routes/articleRoutes.js
@@ -0,0 +1,35 @@
+    // routes/articleRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const Article = require('../models/Article');
+
+// GET all articles
+router.get('/', async (req, res) => {
+    try {
+        const articles = await Article.find();
+        res.json(articles);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// POST create a new article
+router.post('/', async (req, res) => {
+    const article = new Article({
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image
+    });
+
+    try {
+        const newArticle = await article.save();
+        res.status(201).json(newArticle);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// ... Other routes for updating, deleting articles, etc.
+
+module.exports = router;
